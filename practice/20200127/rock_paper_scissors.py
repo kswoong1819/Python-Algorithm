49ec3fd91811e1a6db83b1e27db580ba3b08b9da
woong <kswoong1819@gamil.com>
2020-01-27 23:04:16 +0900

    20200127

---
+++ practice/20200127/rock_paper_scissors.py
@@ -0,0 +1,10 @@
+personA, personB = map(int, input().split(' '))
+
+if personA == 1 and personB == 3:
+    print('A')
+elif personA == 2 and personB == 1:
+    print('A')
+elif personA == 3 and personB == 2:
+    print('A')
+else:
+    print('B')
