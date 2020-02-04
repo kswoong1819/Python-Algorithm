5b5a07b8835e46785d094de579ef7f3343f42e30
woong <kswoong1819@gamil.com>
2020-02-05 00:53:29 +0900

    20200204_2

--- practice/2020_02/20200204/7일차_금속막대_김성웅.py
+++ practice/2020_02/20200204/7일차_금속막대_김성웅.py
@@ -1,23 +1,28 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-
-
 T = int(input())
 
 for t in range(T):
     pair = int(input())
     screw = input().split()
-    pair_list = []
+    pair_list = [[screw[x] for x in range(y, y+2)] for y in range(0, len(screw), 2)]
 
-    for i in range(0, len(screw), 2):
-        pair_1 = [screw[i], screw[i+1]]
-        pair_list.append(pair_1)
-    print(pair_list)
-
-
-    for x in range(pair):
-        pair_list -= pair_list[x]
-        print(pair_list)
-        # for y in pair_list:
-        #     print(y)
+    for y in pair_list:
+        ans = []
+        n = 0
+        ans.append(y)
+        while len(ans) != pair:
+            for i in pair_list:
+                if ans[n][1] == i[0]:
+                    ans.append(i)
+                    n += 1
+                    break
+            else:
+                break
+        if len(ans) == pair:
+            break
+    print('#{} '.format(t + 1), end='')
+    for x in ans:
+        print(' '.join(x), end=' ')
+    print()
---
+++ practice/practice.py
@@ -0,0 +1,2 @@
+matrix = [list(map(str, input().split())) for _ in range(3)]
+print(matrix)
