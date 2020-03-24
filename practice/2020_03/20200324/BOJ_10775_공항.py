4d88c3132444a73c9b31513645d19e360cdde8c6
kswoong1819 <kswoong1819@gmail.com>
2020-03-25 00:25:42 +0900

    20200324_2

--- practice/2020_03/20200324/BOJ_10775_공항.py
+++ practice/2020_03/20200324/BOJ_10775_공항.py
@@ -1,9 +1,34 @@
 import sys
 
 sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+def parent_find(x):
+    if gate[x] == x:
+        return x
+    gate[x] = parent_find(gate[x])
+    return gate[x]
+
+
+def union(x, y):
+    x = parent_find(x)
+    y = parent_find(y)
+    gate[x] = y
+
 
 G = int(input())
 P = int(input())
 plane = []
 for _ in range(P):
-    
+    plane.append(int(input()))
+
+gate = [i for i in range(G + 1)]
+cnt = 0
+for i in plane:
+    re = parent_find(i)
+    if re == 0:
+        break
+    union(re, re - 1)
+    cnt += 1
+
+print(cnt)
