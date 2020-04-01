ce00587f7f7babba2d621a6e98dcdeb22ccfadd4
woong <kswoong1819@gamil.com>
2020-04-01 23:03:38 +0900

    0401

--- practice/2020_04/0401/BOJ_1956_운동.py
+++ practice/2020_04/0401/BOJ_1956_운동.py
@@ -1,8 +1,18 @@
 import sys
-sys.stdin = open('input.txt')
 
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
 from heapq import heappush, heappop
 
-V, E = map(int, input().split())
-road = [] * (E+1)
+def bfs(st):
+    dist = [float('inf')]
 
+
+V, E = map(int, input().split())
+road = [] * (E + 1)
+for i in range(V + 1):
+    a, b, c = map(int, input().split())
+    road[a].append((b, c))
+
+for i in range(V):
+    bfs(i)
