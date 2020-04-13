39f0fb9fb82ae01c0866cc7aad61b92d7dd62967
kswoong1819 <kswoong1819@gmail.com>
2020-04-13 14:02:06 +0900

    0413_0

---
+++ practice/2020_04/0413/BOJ_14499_주사위 굴리기.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('input.txt')
+
+N, M, x, y, K = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(N)]
+move = list(map(int, input().split()))
+
+dice = [0] * 6
+
+top =
+for i in move:
