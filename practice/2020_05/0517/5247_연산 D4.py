6876762e96e627c3d425b18851e745b1e01908bb
kswoong1819 <kswoong1819@gmail.com>
2020-05-17 23:52:34 +0900

    0517

---
+++ practice/2020_05/0517/5247_연산 D4.py
@@ -0,0 +1,33 @@
+import sys
+
+sys.stdin = open('input.txt')
+from collections import deque
+
+T = int(input())
+
+visited = [-1] * 1000001
+for t in range(T):
+    N, M = map(int, input().split())
+    visited[N] = t
+
+    q = deque([[N, 0]])
+    while q:
+        n, cnt = q.popleft()
+        if n == M:
+            print('#{} {}'.format(t + 1, cnt))
+            break
+        if 0 < n + 1 <= 1000000 and visited[n + 1] != t:
+            q.append((n + 1, cnt + 1))
+            visited[n + 1] = t
+
+        if 0 < n - 1 <= 1000000 and visited[n - 1] != t:
+            q.append((n - 1, cnt + 1))
+            visited[n - 1] = t
+
+        if 0 < n * 2 <= 1000000 and visited[n * 2] != t:
+            q.append((n * 2, cnt + 1))
+            visited[n * 2] = t
+
+        if 0 < n - 10 <= 1000000 and visited[n - 10] != t:
+            q.append((n - 10, cnt + 1))
+            visited[n - 10] = t
---
+++ practice/2020_05/0517/5248_그룹 나누기 D3.py
@@ -0,0 +1,30 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+
+def go(n, visited):
+    visited[n] = 1
+    for i in graph[n]:
+        if visited[i] == 0:
+            go(i, visited)
+
+
+T = int(input())
+for t in range(T):
+    N, M = map(int, input().split())
+    graph = [[] for _ in range(N + 1)]
+    nums = list(map(int, input().split()))
+    for i in range(0, M * 2, 2):
+        graph[nums[i]].append(nums[i + 1])
+        graph[nums[i + 1]].append(nums[i])
+
+    cnt = 0
+    visited = [0] * (N + 1)
+    for i in range(1, N + 1):
+        if visited[i]:
+            continue
+        cnt += 1
+        go(i, visited)
+
+    print('#{} {}'.format(t + 1, cnt))
